f2bec8ef784e097f3de0a22ef5d61fa37ad049b0
Ray Yap <rayyap.dev@gmail.com>
2025-06-03 23:03:37 +1000

    Comepleted Q7

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    split = string.split('');
+    result = "";
+    for (let i = split.length - 1; i >= 0; i--){
+        result += split[i];
+    }
+    return result;
 };
 
 // Do not edit below this line
