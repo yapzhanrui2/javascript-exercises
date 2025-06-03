f261f65c2ca53d9da07ef9715b7e7913d52d0b04
Ray Yap <rayyap.dev@gmail.com>
2025-06-03 23:40:13 +1000

    Completed Q11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  num = (temp - 32) * 5/9
+  return Math.round(num * 10)/10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  num = temp * 9/5 + 32
+  return Math.round(num * 10)/10
 };
 
 // Do not edit below this line
