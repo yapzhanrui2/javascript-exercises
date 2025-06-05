a0e9b5ecccf6d1469e72183104d3fdf2a3420407
Ray Yap <rayyap.dev@gmail.com>
2025-06-05 15:59:33 +1000

    Completed Q16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    return arr.reduce((oldest, curr) => {
+        oldestAge = getAge(oldest.yearOfDeath, oldest.yearOfBirth);
+        currentAge = getAge(curr.yearOfDeath, curr.yearOfBirth);
+        
+        return oldestAge > currentAge ? oldest : curr;
+    })
 };
+
+function getAge(death, birth) {
+    if (!death) {
+    death = new Date().getFullYear();
+  }
+    return death-birth
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
