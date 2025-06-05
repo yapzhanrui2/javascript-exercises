4af8c93237a1ec0c828cd24b6dae0a451a146ca7
Ray Yap <rayyap.dev@gmail.com>
2025-06-05 15:26:11 +1000

    Completed Q13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    const cleanedString = string
+    .toLowerCase()
+    .split('')
+    .filter((character) => alphanumerical.includes(character))
+    .join('');
+
+    return cleanedString === cleanedString.split('').reverse().join('');
 };
 
 // Do not edit below this line
