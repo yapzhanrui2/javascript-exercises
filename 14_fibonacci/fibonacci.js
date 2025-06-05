2722021d54bb2a15a2257317da4e59a625d26bc7
Ray Yap <rayyap.dev@gmail.com>
2025-06-05 15:34:40 +1000

    Completed Q14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
 
+    if (typeof n !== 'number'){
+        n = parseInt(n);
+    }
+    if (n < 0){
+        return "OOPS"
+    }
+    if (n <= 1){
+        return n;
+    }
+
+    return fibonacci(n-1) + fibonacci(n-2)
 };
 
 // Do not edit below this line
